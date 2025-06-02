45449b6e8a87d813acfd57811779f77ac4c016b4
hrishd <hrishd@abv.bg>
2025-06-02 10:50:07 +0300

    get contents

---
+++ test.php
@@ -0,0 +1,14 @@
+<!-- dev/JobScraper/test.html -->
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Test Page</title>
+</head>
+
+<body>
+  <h1>This is a static test page</h1>
+  <p>If you see this, fetch_html() worked!</p>
+</body>
+
+</html>
