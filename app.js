70d16bb6547bd3a989614041956573c671c4fb23
hrishd <hrishd@abv.bg>
2025-06-03 14:14:18 +0300

    changes

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ const fs = require('fs');
 
 const allowed = JSON.parse(fs.readFileSync('./allowed.json', 'utf8')); //json data about the allowed urls
 
-function isUrlAllowed(inputUrl) {
-    const hostname = new URL(inputUrl).hostname.replace(/^www\./, '');
+function getURLData(inputUrl) {
+    const hostname = new URL(inputUrl).hostname.replace(/^www\./, '').toLowerCase();
 
-    return allowed.some(obj => {
-        const allowedHost = new URL(obj.base_url).hostname.replace(/^www\./, '');
+    const found = allowed.find(obj => {
+        const allowedHost = obj.host.replace(/^www\./, '').replace(/\/$/, '').toLowerCase();
         return hostname === allowedHost;
     });
+
+    return found || false;
 }
+
 
 (async () => {
     const url = 'https://www.flexjobs.com/remote-jobs/bilingual';
+    const urlData = getURLData(url);
 
-    if (!isUrlAllowed(url)) {
+    if (!urlData) {
         console.log('URL not recognized!');
         return;
     }
