0c7334ac3962d4c97157439ca9cd379d3fa39a43
hrishd <hrishd@abv.bg>
2025-06-03 12:27:22 +0300

    check allowed urls

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const { getPageHTML } = require('./getPageHTML.js');
+const fs = require('fs');
+
+const allowed = JSON.parse(fs.readFileSync('./allowed.json', 'utf8')); //json data about the allowed urls
+
+function isUrlAllowed(inputUrl) {
+    const hostname = new URL(inputUrl).hostname.replace(/^www\./, '');
+
+    return allowed.some(obj => {
+        const allowedHost = new URL(obj.base_url).hostname.replace(/^www\./, '');
+        return hostname === allowedHost;
+    });
+}
 
 (async () => {
-    const url = 'https://empllo.com/';
+    const url = 'https://www.flexjobs.com/remote-jobs/bilingual';
+
+    if (!isUrlAllowed(url)) {
+        console.log('URL not recognized!');
+        return;
+    }
+
     const result = await getPageHTML(url);
-    console.log(result[0] ? 'Success! HTML saved and returned!' : 'Error:' + result[1]);
+    console.log(result[0] ? 'Success! HTML for' + url + ' returned and saved!' : 'Error:' + result[1]);
 })();
